08d8b2ee8b080e881b7c252e8ab3c836f8eef4c1
Eviatar <Eviatar>
2020-04-02 18:41:58 +0300

    .

--- chip8.cpp
+++ chip8.cpp
@@ -23,7 +23,7 @@
 
 void Chip8::loading_buffer_to_memory(const std::vector<uint8_t>& buffer) noexcept
 {
-    std::copy(buffer.begin(), buffer.end(), memory.begin() + Constants::START_LOCATION);
+    std::copy(buffer.begin(), buffer.end(), memory.begin() + C::START_LOCATION);
 }
 
 void Chip8::load_fontset() noexcept
@@ -68,66 +68,184 @@ void Chip8::disassemble_pc() noexcept
     // A 4-bit value, the upper 4 bits of the low byte of the instruction
     const uint8_t  y   = (opcode >> 4) & 0xF;         // y-axis
     // An 8-bit value, the lowest 8 bits of the instruction
-    const uint8_t  kk  = opcode & 0xFF; // byte
+    const uint8_t  kk  = opcode & 0xFF;               // byte
 
     // Mask the first digit
     switch (opcode & 0xF000)
     {
-        case Constants::ZERO_OPCODE:
-            switch (opcode)
+        case C::ZERO_OPCODE:
+            switch (opcode & 0x00FF)
             {
                 // 0NNN implementation not needed because
                 // i'm not trying to emulate RCA 1802 CPU
                 
-                case 0x00E0:
+                case C::OPCODE_00E0:
+                PRINT_DIRECTIVE(C::OPCODE_00E0);
+                break;
 
+                case C::OPCODE_00EE:
+                PRINT_DIRECTIVE(C::OPCODE_00EE);
+                break;
+
+                default:
+                    std::cout << C::MEMORY_OPCODE_ERROR_MSG << std::endl;
                 break;
             }
         break;
 
-        case Constants::ONE_OPCODE:
+        case C::ONE_OPCODE:   // 1NNN
+            PRINT_DIRECTIVE(C::ONE_OPCODE);
         break;
 
-        case Constants::TWO_OPCODE:
+        case C::TWO_OPCODE:   // 2NNN
+            PRINT_DIRECTIVE(C::TWO_OPCODE);
         break;
 
-        case Constants::THREE_OPCODE:
+        case C::THREE_OPCODE: // 3XNN
+            PRINT_DIRECTIVE(C::THREE_OPCODE);
         break;
 
-        case Constants::FOUR_OPCODE:
+        case C::FOUR_OPCODE:  // 4XNN
+            PRINT_DIRECTIVE(C::FOUR_OPCODE);
         break;
 
-        case Constants::FIVE_OPCODE:
+        case C::FIVE_OPCODE:  // 5XY0
+            PRINT_DIRECTIVE(C::FIVE_OPCODE);
         break;
 
-        case Constants::SIX_OPCODE:
+        case C::SIX_OPCODE:   // 6XNN
+            PRINT_DIRECTIVE(C::SIX_OPCODE);
         break;
 
-        case Constants::SEVEN_OPCODE:
+        case C::SEVEN_OPCODE: // 7XNN
+            PRINT_DIRECTIVE(C::SEVEN_OPCODE);
         break;
 
-        case Constants::EIGHT_OPCODE:
+        case C::EIGHT_OPCODE:
+            switch (opcode & 0x000F)
+            {
+                case C::OPCODE_8XY0:
+                    PRINT_DIRECTIVE(C::OPCODE_8XY0);
+                break;
+
+                case C::OPCODE_8XY1:
+                    PRINT_DIRECTIVE(C::OPCODE_8XY1);
+                break;
+
+                case C::OPCODE_8XY2:
+                    PRINT_DIRECTIVE(C::OPCODE_8XY2);
+                break;
+
+                case C::OPCODE_8XY3:
+                    PRINT_DIRECTIVE(C::OPCODE_8XY3);
+                break;
+
+                case C::OPCODE_8XY4:
+                    PRINT_DIRECTIVE(C::OPCODE_8XY4);
+                break;
+
+                case C::OPCODE_8XY5:
+                    PRINT_DIRECTIVE(C::OPCODE_8XY5);
+                break;
+
+                case C::OPCODE_8XY6:
+                    PRINT_DIRECTIVE(C::OPCODE_8XY6);
+                break;
+
+                case C::OPCODE_8XY7:
+                    PRINT_DIRECTIVE(C::OPCODE_8XY7);
+                break;
+
+                case C::OPCODE_8XYE:
+                    PRINT_DIRECTIVE(C::OPCODE_8XYE);
+                break;
+
+                default:
+                    std::cout << C::MEMORY_OPCODE_ERROR_MSG << std::endl;
+                break;
+            }
         break;
 
-        case Constants::NINE_OPCODE:
+        case C::NINE_OPCODE:     // 9XY0
+            PRINT_DIRECTIVE(C::NINE_OPCODE);
         break;
 
-        case Constants::TEN_OPCODE:
+        case C::TEN_OPCODE:      // ANNN
+            PRINT_DIRECTIVE(C::TEN_OPCODE);
         break;
 
-        case Constants::ELEVEN_OPCODE:
+        case C::ELEVEN_OPCODE:   // BNNN
+            PRINT_DIRECTIVE(C::ELEVEN_OPCODE);
         break;
 
-        case Constants::TWELVE_OPCODE:
+        case C::TWELVE_OPCODE:   // CXNN
+            PRINT_DIRECTIVE(C::TWELVE_OPCODE);
         break;
 
-        case Constants::THIRTEEN_OPCODE:
+        case C::THIRTEEN_OPCODE: // DXYN
+            PRINT_DIRECTIVE(C::THIRTEEN_OPCODE);
         break;
 
-        case Constants::FOURTEEN_OPCODE:
+        case C::FOURTEEN_OPCODE:
+            switch (opcode & 0x000F)
+            {
+                case C::OPCODE_EX9E:
+                PRINT_DIRECTIVE(C::OPCODE_EX9E);
+                break;
+
+                case C::OPCODE_EXA1:
+                PRINT_DIRECTIVE(C::OPCODE_EXA1);
+                break;
+
+                default:
+                std::cout << C::MEMORY_OPCODE_ERROR_MSG << std::endl;
+                break;
+            }
         break;
 
-        case Constants::FIFTEEN_OPCODE:
+        case C::FIFTEEN_OPCODE:
+            switch (opcode & 0x00FF)
+            {
+                case C::OPCODE_FX07:
+                PRINT_DIRECTIVE(C::OPCODE_FX07);
+                break;
+
+                case C::OPCODE_FX0A:
+                PRINT_DIRECTIVE(C::OPCODE_FX0A);
+                break;
+
+                case C::OPCODE_FX15:
+                PRINT_DIRECTIVE(C::OPCODE_FX15);
+                break;
+
+                case C::OPCODE_FX18:
+                PRINT_DIRECTIVE(C::OPCODE_FX18);
+                break;
+
+                case C::OPCODE_FX1E:
+                PRINT_DIRECTIVE(C::OPCODE_FX1E);
+                break;
+
+                case C::OPCODE_FX29:
+                PRINT_DIRECTIVE(C::OPCODE_FX29);
+                break;
+
+                case C::OPCODE_FX33:
+                PRINT_DIRECTIVE(C::OPCODE_FX33);
+                break;
+
+                case C::OPCODE_FX55:
+                PRINT_DIRECTIVE(C::OPCODE_FX55);
+                break;
+
+                case C::OPCODE_FX65:
+                PRINT_DIRECTIVE(C::OPCODE_FX65);
+                break;
+
+                default:
+                std::cout << C::MEMORY_OPCODE_ERROR_MSG << std::endl;
+                break;
+            }
         break;
 
         default:
@@ -148,7 +266,7 @@ void Chip8::disassemble_loop() noexcept
 
         // jump in the memory by 2
         // because every instruction is 2 bytes long
-        program_counter += Constants::INSTRUCTION_BYTES;
+        program_counter += C::INSTRUCTION_BYTES;
     }
 }
 
@@ -161,7 +279,7 @@ void Chip8::fetch_opcode() noexcept
 }
 
 Chip8::Chip8(const std::string& path)
-    : memory(Constants::MAXIMUM_BYTES)
+    : memory(C::MAXIMUM_BYTES)
 {
     std::ifstream file(path, std::ios::in     | 
                              std::ios::ate    | 
--- chip8.hpp
+++ chip8.hpp
@@ -8,8 +8,8 @@
 
 #define PRINT_DIRECTIVE(x) puts(#x);
 
-// the C++ way of Defines
-namespace Constants
+// the C++ way of defines
+namespace C // shortcut of Constants
 {
     constexpr auto RESOLUTION_WIDTH  = 64;
     constexpr auto RESOLUTION_HEIGHT = 32;
@@ -20,27 +20,62 @@ namespace Constants
     constexpr auto TIMER_HERTZ        = 60;
 
     // OPCODES - There are 16 different first four bits
-    constexpr auto ZERO_OPCODE       = 0x0000;
-    constexpr auto ONE_OPCODE        = 0x1000;
-    constexpr auto TWO_OPCODE        = 0x2000;
-    constexpr auto THREE_OPCODE      = 0x3000;
-    constexpr auto FOUR_OPCODE       = 0x4000;
-    constexpr auto FIVE_OPCODE       = 0x5000;
-    constexpr auto SIX_OPCODE        = 0x6000;
-    constexpr auto SEVEN_OPCODE      = 0x7000;
-    constexpr auto EIGHT_OPCODE      = 0x8000;
-    constexpr auto NINE_OPCODE       = 0x9000;
-    constexpr auto TEN_OPCODE        = 0xA000;
-    constexpr auto ELEVEN_OPCODE     = 0xB000;
-    constexpr auto TWELVE_OPCODE     = 0xC000;
-    constexpr auto THIRTEEN_OPCODE   = 0xD000;
-    constexpr auto FOURTEEN_OPCODE   = 0xE000;
-    constexpr auto FIFTEEN_OPCODE    = 0xF000;
+    constexpr auto /*      */ ZERO_OPCODE       = 0x0000;
+    constexpr auto /* 1NNN */ ONE_OPCODE        = 0x1000; // Jumps to address NNN. 
+    constexpr auto /* 2NNN */ TWO_OPCODE        = 0x2000; // Calls subroutine at NNN. 
+    constexpr auto /* 3XNN */ THREE_OPCODE      = 0x3000; // Skips the next instruction if VX equals NN.
+    constexpr auto /* 4XNN */ FOUR_OPCODE       = 0x4000; // Skips the next instruction if VX doesn't equal NN.
+    constexpr auto /* 5XY0 */ FIVE_OPCODE       = 0x5000; // Skips the next instruction if VX equals VY. 
+    constexpr auto /* 6XNN */ SIX_OPCODE        = 0x6000; // Sets VX to NN. 
+    constexpr auto /* 7XNN */ SEVEN_OPCODE      = 0x7000; // Adds NN to VX.
+    constexpr auto /*      */ EIGHT_OPCODE      = 0x8000;
+    constexpr auto /* 9XY0 */ NINE_OPCODE       = 0x9000; // Skips the next instruction if VX doesn't equal VY. 
+    constexpr auto /* ANNN */ TEN_OPCODE        = 0xA000; // Sets I to the address NNN. 
+    constexpr auto /* BNNN */ ELEVEN_OPCODE     = 0xB000; // Jumps to the address NNN plus V0. 
+    constexpr auto /* CXNN */ TWELVE_OPCODE     = 0xC000; // Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN. 
+    constexpr auto /* DXYN */ THIRTEEN_OPCODE   = 0xD000; // Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels. 
+                                                          // Each row of 8 pixels is read as bit-coded starting from memory location I; 
+                                                          // I value doesn’t change after the execution of this instruction. 
+                                                          // As described above, VF is set to 1 if any screen pixels are flipped from set to unset when 
+                                                          // the sprite is drawn, and to 0 if that doesn’t happen
+    constexpr auto /*      */ FOURTEEN_OPCODE   = 0xE000;
+    constexpr auto /*      */ FIFTEEN_OPCODE    = 0xF000;
 
-    // OPCODES
-    // zero opcodes:
-    constexpr auto OPCODE_CLS     = 0x00E0; // Clear the display.
-    constexpr auto OPCODE_RET     = 0x00EE; // Return from a subroutine.
+    // ZERO OPCODES:
+    constexpr auto OPCODE_00E0 = 0x0000; // Clear the display.
+    constexpr auto OPCODE_00EE = 0x000E; // Return from a subroutine.
+
+    // EIGHT OPCODES:
+    constexpr auto OPCODE_8XY0 = 0x0000; // Sets VX to the value of VY. 
+    constexpr auto OPCODE_8XY1 = 0x0001; // Sets VX to VX or VY. (Bitwise OR operation) 
+    constexpr auto OPCODE_8XY2 = 0x0002; // Sets VX to VX and VY. (Bitwise AND operation) 
+    constexpr auto OPCODE_8XY3 = 0x0003; // Sets VX to VX xor VY. 
+    constexpr auto OPCODE_8XY4 = 0x0004; // Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't. 
+    constexpr auto OPCODE_8XY5 = 0x0005; // VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't. 
+    constexpr auto OPCODE_8XY6 = 0x0006; // Stores the least significant bit of VX in VF and then shifts VX to the right by 1.
+    constexpr auto OPCODE_8XY7 = 0x0007; // Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't. 
+    constexpr auto OPCODE_8XYE = 0x000E; // Stores the most significant bit of VX in VF and then shifts VX to the left by 1.
+
+    // FOURTEEN OPCODES:
+    constexpr auto OPCODE_EX9E = 0x000E; // Skips the next instruction if the key stored in VX is pressed.
+    constexpr auto OPCODE_EXA1 = 0x0001; // Skips the next instruction if the key stored in VX isn't pressed.
+
+    // FIFTEEN OPCODES:
+    constexpr auto OPCODE_FX07 = 0x0007; // Sets VX to the value of the delay timer. 
+    constexpr auto OPCODE_FX0A = 0x000A; // A key press is awaited, and then stored in VX. 
+    constexpr auto OPCODE_FX15 = 0x0015; // Sets the delay timer to VX. 
+    constexpr auto OPCODE_FX18 = 0x0018; // Sets the sound timer to VX. 
+    constexpr auto OPCODE_FX1E = 0x001E; // Adds VX to I. VF is set to 1 when there is a range overflow (I+VX>0xFFF), and to 0 when there isn't.
+    constexpr auto OPCODE_FX29 = 0x0029; // Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) 
+                                         // are represented by a 4x5 font. 
+    constexpr auto OPCODE_FX33 = 0x0033; // Stores the binary-coded decimal representation of VX, with the most significant of three digits at the address in I,
+                                         // the middle digit at I plus 1, and the least significant digit at I plus 2. 
+    constexpr auto OPCODE_FX55 = 0x0055; // Stores V0 to VX (including VX) in memory starting at address I. 
+                                         // The offset from I is increased by 1 for each value written, but I itself is left unmodified.
+    constexpr auto OPCODE_FX65 = 0x0065; // Fills V0 to VX (including VX) with values from memory starting at address I. 
+                                         // The offset from I is increased by 1 for each value written, but I itself is left unmodified.
+
+    constexpr char const* MEMORY_OPCODE_ERROR_MSG = "Memory OPCODE is wrong!";
 }
 
 class Chip8
@@ -64,9 +99,8 @@ private:
     std::vector<uint8_t> memory;
 
     // pseudo registers
-    uint16_t program_counter = Constants::START_LOCATION;         // PC
-    uint16_t stack_pointer;                                       // SP
-    
+    uint16_t program_counter = C::START_LOCATION; // PC
+    uint16_t* stack_pointer  = nullptr;           // SP
     
     std::array<uint8_t, 16> registers; // VO
     std::array<uint16_t, 24> stack;
@@ -75,8 +109,8 @@ private:
     uint16_t index; // I
 
     // timers - rate 60hz
-    uint16_t delay_timer = Constants::TIMER_HERTZ;
-    uint16_t sound_timer = Constants::TIMER_HERTZ;
+    uint16_t delay_timer = C::TIMER_HERTZ;
+    uint16_t sound_timer = C::TIMER_HERTZ;
 };
 
 #endif
